1e8565e0daa6b3335cbadb0e61f6cfb77daed424
James Alves <james@jamesalves.com.br>
2023-08-30 19:35:34 -0300

    ultima atualizacao

--- aula3/programa.py
+++ aula3/programa.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import psycopg2
 
 dados1 = pd.read_csv('dados/10-08-2023_NEGOCIOSAVISTA.txt',
                      header=0, 
@@ -21,3 +22,19 @@ dadosx = dados1[['CodigoInstrumento','PrecoNegocio']]
 codigos = dadosx.groupby('CodigoInstrumento').mean()
 arquivo = open('dados/saida2.txt', 'w')
 arquivo.write(codigos.to_csv())
+
+conn = psycopg2.connect("dbname=jbnkzhdn host=rajje.db.elephantsql.com user=jbnkzhdn password=bUyS24TieGTMFnp3N49sjlm3J_Zt6owy")
+
+cur = conn.cursor()
+idx = 0
+for item in codigos.itertuples():
+    ## print(item)
+    cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)",(int(item[1]), item[0]))
+    print(f'ta vivo {idx}')
+    if(idx%10 == 0):
+        conn.commit()
+    idx = idx + 1
+
+conn.commit()        
+cur.close()
+conn.close()
